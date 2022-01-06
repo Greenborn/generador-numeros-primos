242ace8068a1c7af13bedc5d9e08d01a2c979e23
berty <lucho.2012.tandil@gmail.com>
2022-01-06 16:44:31 -0300

    Se acelera la velocidad al usar un arreglo gigante para cribar los valores, aunque ahora es una maquina de consumir memoria

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string>
 #include <fstream>
+#include <array>
+#include <list>
+#include <vector>
 
 using namespace std;
 
@@ -10,16 +13,45 @@ using std::endl; using std::string;
 using std::cerr;
 using std::fstream;
 
-bool esPrimo( int numero ){
-    long contador;
-    long max = (numero / 2) + 1;
-    for (contador = 2; contador < max; contador ++){
-        if (numero % contador == 0){
-            return false;
+template < class T, class Alloc = allocator<T> > class list;
+
+void cribar(fstream &outfile, long &fin){
+    long contador = 0;
+ 
+    long numbers_size = fin;
+    long* numbers = NULL;
+    numbers = new long[numbers_size];
+        
+    //Si hay memoria hay que usarla!
+    for (contador = 0; contador < numbers_size; contador++){
+        numbers[contador] = contador;
+    }
+
+    //Se hace la criba
+    long number_size_limit = fin / 2;
+    long it=0;
+    for (contador = 2; contador < number_size_limit; contador++){
+        std::cout << ("Verificando multiplos de: " + to_string(contador) + " \n");
+
+        for (it = contador; it < numbers_size; it += contador){
+            if (numbers[it] % contador == 0 && numbers[it] != contador){ // si es divisible ya lo marcamos pa descartarlo
+                numbers[it] = -1;
+            }
         }
     }
-    
-    return true;    
+
+    //Pasamos los numeros al archivo
+    for (contador = 1; contador < numbers_size; contador++){
+        if(numbers[contador] != -1){
+            string outp = "Número primo: " + to_string(numbers[contador]) + " \n";
+            std::cout << outp;
+
+            string concat = to_string(numbers[contador]) + " ";
+            string text(concat);
+            outfile.write(text.data(), text.size());
+        }
+    }
+
 }
 
 int main(int numArg, char ** arg){          
@@ -32,33 +64,22 @@ int main(int numArg, char ** arg){
     }
 
     string archivo = arg[1];
-    long inicio = std::stol(arg[2]);
-    if (inicio < 2){
-	    std::cout << "Numero de inicio debe ser mayor a 2 \n";   
-        return 0;
-    }
-
-    std::cout << "Se escribirà el archivo:" + archivo + " iniciando cuenta en: " + to_string(inicio) + " \n";
+    long fin = std::stol(arg[2]);
 
     //creacion de un nuevo archivo
     string filename2(archivo);
     fstream outfile;
     outfile.open(filename2, std::ios_base::app);
 
-    long contador = 0;
+    //eliminacion de elemntos de arreglo dinamico
+    using std::cout; using std::endl;
+    using std::array; using std::remove;
+
+    
     if (!outfile.is_open()) {
         cerr << "Error al intentar abrir " << filename2 << '\n';
     } else {
-        for (contador = inicio; contador < 2147483647; contador++){
-            if (esPrimo(contador)){
-                string outp = "Número primo: " + to_string(contador) + " \n";
-                std::cout << outp;
-
-                string concat = to_string(contador) + " ";
-                string text(concat);
-                outfile.write(text.data(), text.size());
-            }
-        }    
+        cribar(outfile, fin);
     }
     cerr << "Finalizado" << endl;
 
